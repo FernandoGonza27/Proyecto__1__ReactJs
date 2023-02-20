334908cfc11168723c3df3cc4ce677ce6864766c
FernandoGonza27 <fg760173@gmail.com>
2023-02-20 01:15:42 -0600

    Advance Carts

---
+++ proyecto-reactjs/src/components/Carts/WebsiteCard.js
@@ -0,0 +1,48 @@
+
+import "./cardsStyle.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSkullCrossbones } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+    const WebsiteCard = ({cart,handleChoice}) => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () =>{
+      navigate(`/products/${cart.products}`)
+      //en este apartado debemos enviar los productos
+    }
+
+    const handleDelete = () =>{
+      handleChoice(cart);
+    }
+    return (
+      <>      
+      <div
+        className="card"
+        key={cart.id}
+        //</>onClick={handleClick}
+      >
+        <div className="card__body">
+          <div className="card__title">
+            <h4>Id the Cart :{cart.id}</h4>
+            <button>              
+              <FontAwesomeIcon 
+              className="card__icon"
+              onClick={handleDelete} 
+              icon={faSkullCrossbones}
+              >Delete
+              </FontAwesomeIcon>
+            </button>
+          </div>
+          <p>total payable is {cart.total}$</p>
+          <a href={"cart.url"} target="_blank" rel="noopener noreferrer">
+            Go to the products
+          </a>
+        </div>
+      </div>
+      
+      </>
+    );
+}
+
+export default WebsiteCard;
