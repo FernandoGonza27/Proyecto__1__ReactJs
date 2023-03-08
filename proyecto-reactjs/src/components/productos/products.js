4dbacee4ee8b38dd783631777a0e4f9ad943ded1
Erick3017 <erickvinicio30@gmail.com>
2023-03-07 20:34:32 -0600

    se termino esta mierda

--- proyecto-reactjs/src/components/productos/products.js
+++ proyecto-reactjs/src/components/productos/products.js
@@ -3,12 +3,12 @@ import axios from "axios";
 import './products.scss'
 import Card from "./cartProducts/cartProducts";
 import List from "./listProducts/listProducts";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const Products = ({
 	cartProducts,
-    setCartProducts,
+	setCartProducts,
 	total,
 	settotal,
 	handleCloseProducts,
@@ -16,59 +16,61 @@ const Products = ({
 	setCountProducts
 
 }) => {
-    const [data, setData] = useState(false);	
+	const [data, setData] = useState(false);
 	useEffect(() => {
 		axios
 			.get("https://dummyjson.com/products")
 			.then(function (response) {
-								
+
 				setData(response.data.products);
 			})
 			.catch(function (error) {
-				
+
 				console.log(error);
 			});
 	}, []);
-	
 
-	const handleClose = () =>{
+
+	const handleClose = () => {
 		handleCloseProducts();
 	}
 
-	return (										
+	return (
+		<div>
+			<div className="close">
+				<button onClick={handleClose}>Close</button>
+			</div>
 			<div className="container">
-	
+
 				<div className="container__cart"  >
-					<div className="container__close">							
-							<button onClick={handleClose}>X</button>
-					</div>
 					<Card
 						cartProducts={cartProducts}
 						setCartProducts={setCartProducts}
 						total={total}
-						settotal={settotal}	
-						countProducts ={countProducts}
-           				setCountProducts ={setCountProducts}																			
-					/>																				
+						settotal={settotal}
+						countProducts={countProducts}
+						setCountProducts={setCountProducts}
+					/>
 				</div>
 				<div className="container__list">
 
 					{
 						data ? <List
-						dataProducts={data}
-						cartProducts={cartProducts}
-						setCartProducts={setCartProducts}
-						total={total}
-						settotal={settotal}
-						countProducts ={countProducts}
-           				setCountProducts ={setCartProducts}				
-						/> :<p>Loading.....</p>
-					}	
+							dataProducts={data}
+							cartProducts={cartProducts}
+							setCartProducts={setCartProducts}
+							total={total}
+							settotal={settotal}
+							countProducts={countProducts}
+							setCountProducts={setCartProducts}
+						/> : <p>Loading.....</p>
+					}
 				</div>
 
-				
-			</div>						
-		
+
+			</div>
+		</div>
+
 	);
 }
 
--- proyecto-reactjs/src/pages/carts.js
+++ proyecto-reactjs/src/pages/carts.js
@@ -1,72 +1,76 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import WebsiteCard from "../components/Carts/WebsiteCard";
 
-import"../components/Carts/cardsStyle.scss";
+import "../components/Carts/cardsStyle.scss";
 
 
 
 const Carts = () => {
-    //const [data, setData] = useState(false);
-    const [webCarts, setwebCarts] = useState([]);
-	const user =localStorage.getItem('account');		
-	let userId=JSON.parse(user).userId;
+	//const [data, setData] = useState(false);
+	const [webCarts, setwebCarts] = useState([]);
+	const user = localStorage.getItem('account');
+	let userId = JSON.parse(user).userId;
 
-	useEffect(() => {  		
+	useEffect(() => {
 		axios
 			.get(`https://dummyjson.com/carts/user/${userId}`)
-			.then(function (response) {			
+			.then(function (response) {
 				setwebCarts(response.data.carts);
-                
+
 
 			})
 			.catch(function (error) {
 				// handle error
 				console.log(error);
-			});	
-		
-					   
+			});
+
+
 	}, []);
-	const handleChoice = (cart) =>{
-		const newCarts= webCarts.filter(card => card.id != cart.id);		
+	const handleChoice = (cart) => {
+		const newCarts = webCarts.filter(card => card.id != cart.id);
 		setwebCarts(newCarts);
 	}
-	const helper =(
+	const helper = (
 		<div className="helper">
 			<h2>You have no pending carts</h2>
 		</div>
 	);
-	
-	
+
+
 	console.log(webCarts.length);
 	console.log(webCarts);
-	if(webCarts.length != 0){
+	if (webCarts.length != 0) {
 		return (
-			<div  className="cart__container">					
-				<h2>Carts of User </h2>			
-				<div className="card-grid">																	
-					{	
-						webCarts ? 					
-							webCarts.map(cart => (
-								<WebsiteCard 
-									key={cart.id} 
-									cart={cart} 
-									handleChoice={handleChoice}						
-								/>
-								
-							))
-						
-						:<p>Loading....</p>
-						
-					}								
+			<div>
+				<div className="cart__container">
+					<div className="title-carts">
+						<h2> Carts of User</h2>
+					</div>
+					<div className="card-grid">
+						{
+							webCarts ?
+								webCarts.map(cart => (
+									<WebsiteCard
+										key={cart.id}
+										cart={cart}
+										handleChoice={handleChoice}
+									/>
+
+								))
+									
+								: <p>Loading....</p>
+
+						}
+					</div>
 				</div>
 			</div>
 		);
-	}else{
-		 return helper;
-		
+	} else {
+		return helper;
+
 	}
-	
+
 }
 
 export default Carts;
