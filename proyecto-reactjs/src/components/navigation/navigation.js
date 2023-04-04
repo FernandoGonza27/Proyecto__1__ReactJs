12c4d6217f7b4cc3949efbcbe7a55f05881c49f5
FernandoGonza27 <fg760173@gmail.com>
2023-04-03 22:31:07 -0600

    navigation ready

--- proyecto-reactjs/src/components/navigation/navigation.js
+++ proyecto-reactjs/src/components/navigation/navigation.js
@@ -1,8 +1,9 @@
-import { useNavigate, NavLink } from "react-router-dom";
+import { useNavigate, NavLink,useLocation } from "react-router-dom";
 import "./navigation.scss"
 import { getAuth, signOut} from "firebase/auth";
 
 const Navigation =() => {
+    let location = useLocation();
     const navigate = useNavigate();     
     const user = JSON.parse(localStorage.getItem("usuario"));
     const handleLogout = async () =>{
@@ -15,14 +16,11 @@ const Navigation =() => {
           // An error happened.
         });     
     }    
-    const handleProducts = async () =>{
-        navigate('/products');  
-        
-    }  
+
     return(
         <div className="navigation">
             <div className="navigation__logo">
-                <img src={"/img/logo__app.png"}/>
+                <img src={"/img/logo__app2.png"}/>
             </div>
             <div className="navigation__menu">
                 <ul>
@@ -31,13 +29,16 @@ const Navigation =() => {
                 </ul>
             </div>
             <div className="logout">
-                <div className="logout__close">
-                    <button onClick={handleLogout} >Logout</button></div>                
-            </div>
-            <div className="logout">
-                <div className="logout__close">
-                    <button onClick={handleProducts} >Products</button></div>                
-            </div>
+                <div className="logout__close">                    
+                    { location.pathname === "/products" ? (                    
+                            <button onClick={()=>navigate('/carts')} >Carts</button>         
+                    ) : (                              
+                            <button onClick={()=>navigate('/products')} >Products</button>                                  
+                    )        
+                    }  
+                    <button onClick={handleLogout} >Logout</button>                      
+                </div>                                                 
+            </div>                            
         </div>
     );
 
